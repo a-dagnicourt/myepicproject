f7d27802dcea0998e86dc0569d40aea18f491057
a.dagnicourt <dagnicourt.a@gmail.com>
2022-03-29 13:56:36 +0200

    buildspace: section 2 done

---
+++ programs/myepicproject/src/lib.rs
@@ -0,0 +1,62 @@
+use anchor_lang::prelude::*;
+
+declare_id!("FDwxAQpu7gYXx3abuFXk4ttANwPR28EWCLmboNFvNNTR");
+
+#[program]
+pub mod myepicproject {
+    use super::*;
+    pub fn start_stuff_off(_ctx: Context<StartStuffOff>) -> Result<()> {
+        let base_account = &mut _ctx.accounts.base_account;
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    // The function now accepts a gif_link param from the user. We also reference the user from the Context
+    pub fn add_gif(_ctx: Context<AddGif>, gif_link: String) -> Result<()> {
+        let base_account = &mut _ctx.accounts.base_account;
+        let user = &mut _ctx.accounts.user;
+
+        // Build the struct.
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+
+        // Add it to the gif_list vector.
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Add the signer who calls the AddGif method to the struct so that we can save it
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
+}
